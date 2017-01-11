c11d9092371b29910f688bdff4e6eb04e37ccf87
Charles Peden <ccbpeden@warpmail.net>
2017-01-11 10:16:53 -0800

    update scripts.js to comment out non-applicable code & to add document ready function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business logic:
+
 var add = function(number1, number2){
 	return number1 + number2;
 };
@@ -14,68 +16,72 @@ var divide = function(number1, number2){
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+//Everything below this line is user interface logic:
+$(document).ready(function()) {
+	var number1 = parseInt(prompt("Enter a number:"));
+	var number2 = parseInt(prompt("Enter another number:"));
 
-alert("Add:" + add(number1, number2));
-alert("Subtract:" + subtract(number1, number2));
-alert("Multiply:" + multiply(number1, number2));
-alert("Divide:" + divide(number1, number2));
+	alert("Add:" + add(number1, number2));
+});
+
+// alert("Subtract:" + subtract(number1, number2));
+// alert("Multiply:" + multiply(number1, number2));
+// alert("Divide:" + divide(number1, number2));
 
 
 
 
 // BMI Calculator
-var height = parseFloat( prompt("How tall are you? (inch)"));
-var weight = parseFloat( prompt("How much do you weigh? (pound)"));
+// var height = parseFloat( prompt("How tall are you? (inch)"));
+// var weight = parseFloat( prompt("How much do you weigh? (pound)"));
 
-var heightConverter = function(height){
-	return (height * .0254).toPrecision(4);
+// var heightConverter = function(height){
+	// return (height * .0254).toPrecision(4);
 
-};
+// };
 
-var weightConverter = function(poundWeight){
-	return (poundWeight * .45).toPrecision(4);
-};
+// var weightConverter = function(poundWeight){
+	// return (poundWeight * .45).toPrecision(4);
+// };
 
-var BMI = function(mHeight, kgWeight){
-	return (kgWeight/(mHeight * mHeight)).toPrecision(4);
-};
-
-
-alert(heightConverter(height) + "m");
-alert(weightConverter(weight) + "kg");
+// var BMI = function(mHeight, kgWeight){
+	// return (kgWeight/(mHeight * mHeight)).toPrecision(4);
+// };
 
 
-alert("Your BMI is " + BMI(heightConverter(height), weightConverter(weight)) + ".");
+// alert(heightConverter(height) + "m");
+// alert(weightConverter(weight) + "kg");
+
+
+// alert("Your BMI is " + BMI(heightConverter(height), weightConverter(weight)) + ".");
 
 
 // Fahrenheit to Celsius Converter
 
-var CStemp = function(FHtemp) {
-	return ((FHtemp - 32)/1.8).toFixed(2);
-};
+// var CStemp = function(FHtemp) {
+	// return ((FHtemp - 32)/1.8).toFixed(2);
+// };
 
-alert(CStemp(30));
+// alert(CStemp(30));
 
 // grams to ounces Converter
 
 
-var userInputGrams = parseFloat(prompt("Type the weight in grams."));
+// var userInputGrams = parseFloat(prompt("Type the weight in grams."));
 
-var ozMeasure = function(gMeasure) {
-	return (gMeasure / 28.35).toFixed(2);
-};
+// var ozMeasure = function(gMeasure) {
+	// return (gMeasure / 28.35).toFixed(2);
+// };
 
-alert("The result is:" + ozMeasure(userInputGrams) + " in ounces.");
+// alert("The result is:" + ozMeasure(userInputGrams) + " in ounces.");
 
 // tsp to ML Converter
 
 
-var tspInput = prompt("Please enter the number of TSP:");
+// var tspInput = prompt("Please enter the number of TSP:");
 
-var tspML = function(tspInput) {
-	return (tspInput * 4.93).toFixed(2);
-}
+// var tspML = function(tspInput) {
+	// return (tspInput * 4.93).toFixed(2);
+// }
 
-alert("The amount is " + tspML(tspInput) + "in milliliters.");
+// alert("The amount is " + tspML(tspInput) + "in milliliters.");
